a3f4b779acbeb6e2f37017a79869b63878900993
joehinmoro <joehinmoro@gmail.com>
2022-06-02 21:35:04 +0100

    implemented code logic

--- index.js
+++ index.js
@@ -21,3 +21,74 @@ const createInterface = () => {
         output: process.stdout,
     });
 };
+const promptUsername = (next) => {
+    const rl = createInterface();
+    const questionText = `\nWelcome to "Guess the Integer".
+You'll guess an integer
+between 0(inclusive) and an interger.
+
+What is your username?
+
+`;
+    rl.question(questionText, (anwser) => {
+        username = anwser;
+        rl.close();
+
+        next();
+    });
+};
+
+const setReviewMode = () => {
+    const rl = createInterface();
+    const questionText = `\nReview mode reveals the anwser
+so that the reviewer can guess
+correctly or wrongly at will in order
+to review the code logic
+Press 1 to turn on review mode:
+Press any other key to it turn off.
+`;
+    rl.question(questionText, (anwser) => {
+        if (anwser == 1) {
+            reviewMode = true;
+        }
+        rl.close();
+        newLevel();
+    });
+};
+
+const newLevel = () => {
+    // increment current level and set correct anwser
+    // as a random number based on the range (0, currentlevel)
+    const correctAnswer = correctNumSelector(++currentLevel);
+
+    const rl = createInterface();
+    const questionText = `\nLevel ${currentLevel}.
+Guess an Integer between 0 and ${currentLevel}:
+${reviewMode ? `Review Mode: correct answer is ${correctAnswer}` : ""}
+`;
+    rl.question(questionText, (answer) => {
+        answer = parseInt(answer);
+        if (answer === correctAnswer) {
+            const correctResponse = `\nCORRECT!!!
+You guessed ${answer}
+and the correct answer is ${correctAnswer}
+`;
+            console.log(correctResponse);
+            rl.close();
+            score++;
+            newLevel();
+        } else {
+            const failureResponse = `\nWRONG!!!
+You guessed ${answer}
+and the correct answer is ${correctAnswer}.
+\n${username}, you scored ${score}
+TRY AGAIN!!!
+`;
+            console.log(failureResponse);
+            rl.close();
+        }
+    });
+};
+
+promptUsername(setReviewMode);
+// console.log(username, reviewMode);
