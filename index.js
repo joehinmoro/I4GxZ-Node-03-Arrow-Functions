57ff4cf3f169f747a493b83c55ce66fdd0489c7c
joehinmoro <joehinmoro@gmail.com>
2022-06-03 09:50:08 +0100

    completed refactoring and documentation

--- index.js
+++ index.js
@@ -24,9 +24,11 @@ const promptInterface = () => {
     });
 };
 
-//
-const promptUsername = (next) => {
+// function to start game and prompt username
+const startGame = () => {
+    // create prompt interface
     const rl = promptInterface();
+    // prompt text
     const questionText = `\nWelcome to "Guess the Integer".
 You'll be guessing an integer
 between 1 and another integer.
@@ -34,16 +36,22 @@ between 1 and another integer.
 What is your username?
 
 `;
+    // prompt for username
     rl.question(questionText, (anwser) => {
-        username = anwser;
+        // close prompt interface
         rl.close();
-
-        next();
+        // set username global var to prompt response
+        username = anwser;
+        // call the (optional) review mode function
+        setReviewMode();
     });
 };
 
+// function to toggle review mode (expose answer in prompt)
 const setReviewMode = () => {
+    // create prompt interface
     const rl = promptInterface();
+    // prompt text
     const questionText = `\nReview mode reveals the anwser
 so that the reviewer can guess
 correctly or wrongly at will in order
@@ -52,50 +60,70 @@ to review the code logic
 Press 1 to turn on review mode:
 Press any other key to it turn off.
 `;
+    // prompt to enable review mode
     rl.question(questionText, (anwser) => {
+        // close prompt interface
+        rl.close();
+        // toggle on review mode if prompt response is == 1
         if (anwser == 1) {
             reviewMode = true;
         }
-        rl.close();
+
+        // call game level function
         newLevel();
     });
 };
 
+// define game level function
 const newLevel = () => {
-    // increment current level and set correct anwser
-    // as a random number based on the range (0, currentlevel)
+    // set correct answer global var randomly betweeen 1 and range
     const correctAnswer = correctNumSelector(range);
-
+    // create prompt interface
     const rl = promptInterface();
+    // prompt text
     const questionText = `\nLevel ${currentLevel}.
 Guess an Integer between 1 and ${range}:
 ${reviewMode ? `Review Mode: correct answer is ${correctAnswer}` : ""}
 `;
+    // prompt for user's answer
     rl.question(questionText, (answer) => {
+        // close prompt interface
+        rl.close();
+        // parse user's answer to integer
         answer = parseInt(answer);
+
+        // if user's answer is correct:
         if (answer === correctAnswer) {
-            const correctResponse = `\nCORRECT!!!
+            // define correct answer message
+            const correctMessage = `\nCORRECT!!!
 You guessed ${answer}
 and the correct answer is ${correctAnswer}
 `;
-            console.log(correctResponse);
-            rl.close();
+            // print correct answer message
+            console.log(correctMessage);
+            // increment score var (global)
             score++;
+            // increment level var (global)
             currentLevel++;
+            // increment range var (global)
             range++;
+            // call game level function again for next level
             newLevel();
-        } else {
+        }
+        // if user's answer is wrong:
+        else {
+            // define wrong answer message
             const failureResponse = `\nWRONG!!!
 You guessed ${answer}
 and the correct answer is ${correctAnswer}.
-\n${username}, you scored ${score}
+\n${username}, you scored ${score}.
 TRY AGAIN!!!
 `;
+            // print wrong answer message
             console.log(failureResponse);
-            rl.close();
         }
     });
 };
 
-promptUsername(setReviewMode);
-// console.log(username, reviewMode);
+// start game
+startGame();
